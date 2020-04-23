e7c5cc0b959756e2eba022fa052db92eedaa458d
Elliot Blanchard <public@elliotblanchard.com>
2020-04-23 21:21:27 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.destroy_all
+      @@all = []
+    end
+
+    def save
+      @@all << self
+    end
+
+    def self.create(name)
+      self.new(name)
+    end
+end
