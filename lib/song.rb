05335f021528e172bbda152402ebe983f1b1527c
Elliot Blanchard <public@elliotblanchard.com>
2020-04-23 21:17:36 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Song
+  attr_accessor :name
+
+    @@all = []
+
+    def initialize(name,artist,genre)
+      @name = name
+      #@artist = artist
+      #@genre = genre
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.destroy_all
+      @@all = []
+    end
+
+    def save
+      @@all << self
+    end
+end
