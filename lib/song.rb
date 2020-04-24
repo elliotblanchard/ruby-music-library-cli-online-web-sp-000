fbad29873fb3a41815025d0e9fdb881b22d4f510
Elliot Blanchard <public@elliotblanchard.com>
2020-04-24 02:45:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
     def self.new_from_filename(filename)
       parsed_filename = filename.split(" - ")
       new_song = self.new(parsed_filename[1])
-      new_song.artist_name = parsed_filename[0]
+      #new_song.artist_name = parsed_filename[0]
       new_song.artist.add_song(new_song)
       new_song
     end
