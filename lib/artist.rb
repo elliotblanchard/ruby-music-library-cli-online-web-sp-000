3a1efd4d98ea928ddf843ee5c65e322d9ba44026
Elliot Blanchard <public@elliotblanchard.com>
2020-04-24 00:21:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,7 +28,7 @@ class Artist
     end
 
     def add_song(song)
-      if (song.artist == nil) && (@songs.include? song == false)
+      if song.artist == nil
         @songs << song
         song.artist = self
       end
