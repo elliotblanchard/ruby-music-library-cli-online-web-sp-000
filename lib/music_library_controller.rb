2899e3734e0234c6e5e657d2765075406f828fae
Elliot Blanchard <public@elliotblanchard.com>
2020-04-24 03:05:23 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
 class MusicLibraryController
+  attr_accessor :path
 
+    def initialize(path="./db/mp3s/")
+      importer = MusicImporter.new(path)
+      importer.
+    end
 end
