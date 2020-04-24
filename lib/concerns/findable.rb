ee98095bb3273239df07d10d5778ba2d0949c832
Elliot Blanchard <public@elliotblanchard.com>
2020-04-24 02:02:35 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,4 +1,6 @@
-module Concerns::Findable
+module Concerns
+
+  module Findable
 
     def find_by_name(name)
       self.all.detect {|a| a.name == name}
@@ -11,5 +13,7 @@ module Concerns::Findable
         self.create(name)
       end
     end
+
+  end
 
 end
