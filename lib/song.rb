e0be3d446b5c841b3fa105fbce381a587ce98656
Elliot Blanchard <public@elliotblanchard.com>
2020-04-24 01:19:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
     def initialize(name,artist=nil,genre=nil)
       @name = name
       self.artist = artist if artist
-      @genre = genre
+      self.genre = genre if genre
       @@all << self
     end
 
@@ -23,6 +23,7 @@ class Song
 
     def genre=(genre)
       self.genre = genre
+      genre.add_song(self)
     end
 
     def genre
