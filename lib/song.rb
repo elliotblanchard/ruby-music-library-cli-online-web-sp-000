4216da2d7ada80cfe336b025c6bd66234214fc73
Elliot Blanchard <public@elliotblanchard.com>
2020-04-24 02:51:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,11 +48,19 @@ class Song
       self.new(name)
     end
 
+    def artist_name=(artist_name)
+      @artist_name = Artist.find_or_create_by_name(artist_name)
+      @artist = @artist_name
+    end
+
+    def artist_name
+      @artist_name
+    end
+
     def self.new_from_filename(filename)
       parsed_filename = filename.split(" - ")
       new_song = self.new(parsed_filename[1])
-      binding.pry
-      #new_song.artist_name = parsed_filename[0]
+      new_song.artist_name = parsed_filename[0]
       #new_song.artist.add_song(new_song)
       new_song
     end
