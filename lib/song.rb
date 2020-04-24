29d11624c7a2b2c9b31ec063787dd36806240db2
Elliot Blanchard <public@elliotblanchard.com>
2020-04-24 02:59:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -71,7 +71,7 @@ class Song
       genre_only = parsed_filename.split(".")
       new_song = self.new(parsed_filename[1])
       new_song.artist_name = parsed_filename[0]
-      new_song.genre_name = parsed_filename[2]
+      new_song.genre_name = genre_only[0]
       new_song.artist.add_song(new_song)
       new_song.genre.add_song(new_song)
       new_song
