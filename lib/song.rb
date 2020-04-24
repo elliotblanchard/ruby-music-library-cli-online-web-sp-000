b023e6e316b9aefe108a14f7cc7a5db35ee86ab1
Elliot Blanchard <public@elliotblanchard.com>
2020-04-24 01:55:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,9 +46,9 @@ class Song
       self.new(name)
     end
 
-    def self.find_by_name(name)
-      @@all.detect{|a| a.name == name}
-    end
+    #def self.find_by_name(name)
+    #  @@all.detect{|a| a.name == name}
+    #end
 
     def self.find_or_create_by_name(name)
       if self.find_by_name(name)
